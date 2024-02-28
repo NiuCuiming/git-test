8a3485e3457b8c2de5ceb980d78b439eb8021b29
anu <578277507@qq.com>
2024-02-28 19:58:07 +0800

    commit

--- src/main/java/cn/anu/demo/HelloWord2.java
+++ src/main/java/cn/anu/demo/HelloWord2.java
@@ -1,1 +1,21 @@
 
+package cn.anu.demo;
+
+public class HelloWord2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git! --dev ");
+        System.out.println("hello git! --master dev ");
+        System.out.println("hello git! --master dev ");
+        System.out.println("hello git! --master to dev & remote ");
+        System.out.println("hello git! --master to dev & remote2 ");
+        System.out.println("hello git! --master to dev & remote3 ");
+        System.out.println("hello git! --remote dev to  master");
+        System.out.println("hello git! --master to dev & remote2 ");
+        System.out.println("hello git! --master to dev & remote3 ");
+    }
+}
+
