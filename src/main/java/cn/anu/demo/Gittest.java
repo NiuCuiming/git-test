0a70a37f76876359eca46b4efcf1c4fd4cc590f7
anu <578277507@qq.com>
2024-02-28 18:53:46 +0800

    git-test four commit dev

--- src/main/java/cn/anu/demo/Gittest.java
+++ src/main/java/cn/anu/demo/Gittest.java
@@ -6,6 +6,7 @@ public class Gittest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git! --dev ");
     }
 }
 
